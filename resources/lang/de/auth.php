5a4baf9398ac6c0c1112ba12c8af18a1cb6a115e
Philipp <philipp.esswein@me.com>
2020-09-01 09:06:39 +0200

    Create auth.php

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'sign_in' => 'Anmelden',
+    'go_to_login' => 'Zum Login wechseln',
+    'failed' => 'Kein Account mit den angegebenen Informationen gefunden.',
+
+    'forgot_password' => [
+        'label' => 'Passwort vergessen?',
+        'label_help' => 'E-Mail Adresse des Accounts angeben um weitere Informationen zu erhalten.',
+        'button' => 'Passwort anfordern',
+    ],
+
+    'reset_password' => [
+        'button' => 'Zurücksetzen und einloggen',
+    ],
+
+    'two_factor' => [
+        'label' => '2-Faktor Authentifizierung',
+        'label_help' => 'Dieser Account benötigt eine zweite Authentifizierung.',
+        'checkpoint_failed' => 'Der Token ist falsch.',
+    ],
+
+    'throttle' => 'Zu viele Versuche. Bitte in :seconds Sekunden erneut versuchen.',
+    'password_requirements' => 'Das Passwort muss mindestens 8 Zeichen lang sein.',
+    '2fa_must_be_enabled' => 'Ein Administrator hat diesen Account auf 2-Faktor Authentifizierung umgestellt.',
+];
